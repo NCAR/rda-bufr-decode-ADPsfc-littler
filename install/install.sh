4217aca555ae1c48dd26d51de0e427d3aedec462
Thomas Cram <tcram@ucar.edu>
2020-01-30 15:17:32 -0700

    Updated install script

--- install/install.sh
+++ install/install.sh
@@ -2,9 +2,9 @@
 #  ------------------------------------------------------------------------
 #  This script will make the following executables:
 #
-#  bufr_sfc2ob.x:  read ADP BUFR land surface station input files and
+#  bufr_sfc2ob.x:  read ADPSFC BUFR input files and
 #                  write output to formatted ASCII files
-#  bufr_ship2ob.x: read ADP BUFR ship observation input files and 
+#  bufr_ship2ob.x: read SFCSHIP BUFR input files and 
 #                  write output to formatted ASCII files
 #  dumpbufr.x:     used to dump all contents of a BUFR file.
 #  ------------------------------------------------------------------------
