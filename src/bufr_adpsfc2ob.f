0356898d07b4baad9e15cd96a8cc763a28eafae2
Thomas Cram <tcram@ucar.edu>
2024-06-13 14:37:17 -0600

    updated adpsfc

--- src/bufr_adpsfc2ob.f
+++ src/bufr_adpsfc2ob.f
@@ -1,48 +1,48 @@
-        PARAMETER       ( MXMN = 8 )
-        PARAMETER       ( MXLV = 86 )
-        PARAMETER       ( NVAR = 18 )
-        PARAMETER       ( NSTR = 5 )
+      use moda_tababd
 
-        COMMON /BITBUF/ MAXBYT,IBIT,IBAY(5000),MBYT(32),MBAY(5000,32)
+      PARAMETER (MXMN = 10)
+      PARAMETER (MXLV = 255)
 
-        REAL*8          r8arr ( MXMN, MXLV ), r8arr2(MXMN, MXLV ),
-     +                  r8arr3 ( MXMN, MXLV ), r8arr4(MXMN, MXLV ),
-     +                  r8arr5 (MXMN,MXLV) 
+      REAL*8 idarr(MXMN, MXLV), 
+     +       locarrt(MXMN, MXLV),
+     +       locarrh(MXMN, MXLV), 
+     +       locarrv(MXMN, MXLV),
+     +       obsarr(MXMN,MXLV),
+     +       obsarrh(MXMN,MXLV) 
 
-        PARAMETER       ( MXBF = 16000 )
+c  BUFR mnemonics
+      CHARACTER*40 idstr, locstrt, locstrh, locstrv, obstr, obstrh
+      DATA idstr  /'WMOB WMOS RPID SMID STSN SSTN LSTN      '/
+      DATA locstrt/'YEAR MNTH DAYS HOUR MINU                '/
+      DATA locstrh/'CLAT CLON CLATH CLONH                   '/
+      DATA locstrv/'SELV PRES PMSL                          '/
+      DATA obstr  /'WDIR WSPD TMDB TMDP                     '/
+      DATA obstrh /'REHU TP03 TP24 HOCB CLTP                '/
 
-        parameter(iu=9,iou=10,nz=999999)
+      parameter(iu=9, iou=10, lunit=11)
+      parameter(nz=999999)
+      parameter(dumm=99999.9)
 
-        dimension pr(nz),tt(nz),td(nz),dslp(nz)
-        integer  xht,nlev,iargc,n,minu,k
-        real  xpr,xu,xv
-        real  temp,v(nz),zx(nz),d(nz)
-        real  lat(nz), lon(nz), ter(nz)
-        real xt,xtd
-        character*30 fin,fout
-        character*10  date_tag,date(nz)
-        character*6 dname(nz),staid(nz),M20
-        character   argv*300,minute*2,M11*2,mins(nz)*2
-        character*12 ilev,xy,xm,xd,xh,xmin,M5,M6,M7,M8
-        character*12 M10,M0,M1,M2,min,M3,M4,xn1,xn2,xn3,xn4,M9
-        real wlon,elon,slat,nlat
+      real wmob, wmos, rpid, smid, stsn, sstn, lstn
+      real selv, pres, pmsl
+      real wspd, wdir, tmdb, tmdp
+      real lat, lon
+      real wlon, elon, slat, nlat
 
-        CHARACTER       cbfmsg*(MXBF),
-     +                  csubset*8, inf*200, outstg*200
+      integer nlev, iargc, n
+      integer i, idate
 
-        CHARACTER*80   ostr(NSTR)
+      character*8 csubset
+      character*200 inf
+      character*30 fout
+      character*10 date_tag, date
+      character*6 dname
+      character*300 argv
+      character*2 mins
 
-        INTEGER         ibfmsg ( MXBF / 4 ), ln, code,y,z,i,idate
-
-        LOGICAL         msgok
-
-        EQUIVALENCE     ( cbfmsg (1:4), ibfmsg (1) )
-
-        ostr(1)='WMOB WMOS RPID CLAT CLON'
-        ostr(2)='SELV YEAR MNTH DAYS HOUR'
-        ostr(3)='MINU PRES PMSL WDIR WSPD'
-        ostr(4)='TMDB TMDP REHU TP03 TP24'
-        ostr(5)='HOCB CLTP'
+      INTEGER lun, il, im
+      CHARACTER*80 desc
+      CHARACTER*40 adpsfcname, adpsfcid, adpsfcsource
 
 C*-----------------------------------------------------------------------
 c*    Read the command-line arguments
@@ -70,205 +70,207 @@ c*
             elon = 180.
           END IF
         ELSE
-          write(*,*) 'Usage: bufr_sfc2ob.x gdas.adpsfc.t<HH>z.
-     +<YYYYMMDD>.bufr.be <YYYYMMDDHH> west_lon east_lon 
+          write(*,*) 'Usage: bufr_adpsfc2ob.x gdas.adpsfc.t<HH>z.
+     +<YYYYMMDD>.bufr <YYYYMMDDHH> west_lon east_lon 
      +south_lat north_lat'
           STOP
         END IF
 
 C*-----------------------------------------------------------------------
+C*    Open BUFR input file
+      OPEN (UNIT=lunit, FILE=inf, form='unformatted')
 
-C*      Open the BUFR messages file.
+C*    Open output file
+      fout= "adpsfc"//date_tag//'.obs'
+      open(iou,file=fout, status='unknown', form='formatted')
+      write(iou, fmt='(a10)') date_tag
 
-c*        write(*,*) 'enter input BUFR file?'
-c*        read(*,'(a)') inf 
-c*        write(*,*) 'Date_tag (YYYYMMDDHH) : '
-c*        read(*,fmt='(a10)') date_tag
+      adpsfcsource='NCEP GDAS BUFR ADPSFC observations      '
 
-        fout= "Surface"//date_tag//'.obs'
+      nlev = 1
+      isurf = 1
+      ibogus = 0
+      date='MMMMMMMMMM'
+      mins='MM'
+      staid='MMMMMM'
+      dname=' SYNOP'
+      lat=dumm
+      lon=dumm
+      selv=dumm
+      pmsl=dumm
+      pres=dumm
+      zx=dumm
+      tmdb=dumm
+      tmdp=dumm
+      wdir=dumm
+      wspd=dumm
 
-        OPEN  ( UNIT = 11, FILE =inf,form='unformatted' )
+C* Connect BUFR file to the BUFRLIB software for input operations.
+      CALL OPENBF(lunit, 'IN', lunit)
 
-C*      Open the BUFR tables file.
+C*    Specify that we would like IDATE values returned using 10 digits
+C*    (i.e. YYYYMMDDHH ).
+      CALL DATELEN(10)
 
-C*        OPEN  ( UNIT = 12, FILE = 'bufrtab.example' )
+c  Include code and flag table information from master BUFR tables
+      CALL codflg('Y')
 
-C*      Open output file
+C*-----------------------------------------------------------------------
+c  Loop through BUFR subsets
+      DO WHILE(.true.)
 
-        open(iou,file=fout,status='unknown',form='formatted')
+c       Get file ID (lun) associated with the BUFR file
+        CALL status(lunit, lun, il, im)
 
-        iflag = 0
-        nlev = 1
-        dumm=99999.9
+        call readns(lunit, csubset, idate, ierr)
+        call ufbcnt(lunit, irec, isub)
 
-        isurf = 1
-        ibogus = 0
-        do k=1,nz
-          date(k)='MMMMMMMMMM'
-          mins(k)='MM'
-          staid(k)='MMMMMM'
-          dname(k)=' SYNOP'
-          ter(k) = dumm
-          dslp(k)= dumm
-          pr(k)=dumm
-          zx(k)=dumm
-          tt(k)=dumm
-          td(k)=dumm
-          d(k)=dumm
-          v(k)=dumm
-        enddo
+c        print'(''MESSAGE: '',A8,2(2X,I6),i12 )',
+c     +           csubset,irec,isub,idate
 
+        if(csubset .eq. 'NC000007') then
+            dname=' METAR'
+        endif
 
-C*      Associate the tables file with the messages file, and identify
-C*      the latter to the BUFRLIB software.
+        write(date, '(I10)') idate
 
-        CALL OPENBF  ( 11, 'IN', 11 )
+        IF(ierr .eq. -1) THEN
+          write(*,*) '....all records read, Exit'
+          CALL CLOSBF(lunit)
+          GOTO 2000
+        END IF
 
-C*      Specify that we would like IDATE values returned using 10 digits
-C*      (i.e. YYYYMMDDHH ).
+C* Read data values into arrays
 
-        CALL DATELEN  ( 10 )
-     
-        ln=0 
+        CALL UFBINT(lunit, idarr, MXMN, MXLV, nlev, idstr)
+        CALL UFBINT(lunit, locarrt, MXMN, MXLV, nlev, locstrt)
+        CALL UFBINT(lunit, locarrh, MXMN, MXLV, nlev, locstrh)
+        CALL UFBINT(lunit, locarrv, MXMN, MXLV, nlev, locstrv)
+        CALL UFBINT(lunit, obsarr, MXMN, MXLV, nlev, obstr)
+        CALL UFBINT(lunit, obsarrh, MXMN, MXLV, nlev, obstrh)
 
-        DO WHILE  ( .true. )
+        if (ibfms(nlocarr(5,1)) .eq. 1) then
+           mins='00'
+        else
+           write (mins, FMT='(I2)') int(locarrt(5,1))
+        endif
 
-C*          Read the next BUFR message.
+c  Get Table D index for csubset mnemonic, and get the description
+        CALL nemtab(lun, csubset, idn, tab, n)
+        desc=tabd(n, lun)(16:70)
+        write(adpsfcname, '(A40)') desc(15:)
 
-           call readns(11,csubset,idate,ierr)
-C           code = IUPBS1(MBAY,33) 
-C            write(*,*)' idate: ',idate,'  ',csubset,' ',code
-c            write(*,*)' idate: ',idate,'  ',csubset
-            IF  ( ierr .eq.  -1 )  THEN
-                write(*,*) '....all records read, Exit'
-                CALL CLOSBF  ( 11 )
-                Goto 1000
-            END IF
+C*-----------------------------------------------------------------------
+c  Prepare output
+        CALL get_val(idarr(1,1), wmob)
+        CALL get_val(idarr(2,1), wmos)
+        CALL get_val(idarr(3,1), rpid)
+        CALL get_val(idarr(4,1), smid)
+        CALL get_val(idarr(5,1), stsn)
+        CALL get_val(idarr(6,1), sstn)
+        CALL get_val(idarr(7,1), lstn)
 
-            msgok = .true.
+        CALL get_val(locarrv(1,1), selv)
+        CALL get_val(locarrv(2,1), pres)
+        CALL get_val(locarrv(3,1), pmsl)
+        CALL get_val(obsarr(1,1), wdir)
+        CALL get_val(obsarr(2,1), wspd)
+        CALL get_val(obsarr(3,1), tmdb)
+        CALL get_val(obsarr(4,1), tmdp)
+        CALL get_lat_lon(locarrh(1,1), locarrh(3,1), lat)
+        CALL get_lat_lon(locarrh(2,1), locarrh(4,1), lon)
 
-
-            DO WHILE  ( msgok )
-
-
-C*            At this point, we have a data subset within the
-C*            internal arrays of BUFRLIB, and we can now begin
-C*            reading actual data values:
-
-
-              CALL UFBINT  ( 11, r8arr, MXMN, MXLV, nlv, ostr(1))
-              CALL UFBINT  ( 11, r8arr2, MXMN, MXLV, nlv, ostr(2))
-              CALL UFBINT  ( 11, r8arr3, MXMN, MXLV, nlv, ostr(3))
-              CALL UFBINT  ( 11, r8arr4, MXMN, MXLV, nlv, ostr(4))
-              CALL UFBINT  ( 11, r8arr5, MXMN, MXLV, nlv, ostr(5)) 
-          
-            minu=int(r8arr3(1,1)) 
-            write (unit=minute, FMT='(I2)') minu 
-            DO k=1,2
-               IF ( minute (k:k) .eq. ' ') THEN
-                 minute (k:k) = '0'
-               ENDIF
-            ENDDO
-            DO z = 1,1 
-              WRITE (UNIT=outstg, FMT='(I10,1X,A8,1X,F4.1,1X,F6.1,1X,A6, 
-     +          1X,F5.1,1X,F6.1,1x,F6.1,1X,F6.1,
-     +          4(1X,F4.1),2(1X,F8.1),7(1x,F5.1),1x,F7.1,1x,F4.1)') 
-     +          idate,csubset,
-     +          (r8arr(i,z), i = 1,5),(r8arr2(i,z), i = 1,5),
-     +          (r8arr3(i,z), i = 1,5),(r8arr4(i,z), i = 1,5),
-     +          (r8arr5(i,z), i = 1,2)
-              DO y = 1,156
-               IF ( outstg (y:y) .eq. '*') THEN
-                 outstg (y:y) = 'm'
-               ENDIF
-              ENDDO
- 
-              read(outstg,21) M10,M0,M20,M1,M2,M3,
-     &           M4,M5,M6,M7,M8,M9
-              read(minute,22) M11
-!             write(*,*)M10,M0,M1,M2,M3,M4,M5,M6,M7,M8,M9
-21            format(A10,1X,A8,13X,A6,A6,1X,A6,1X,A6,
-     &         28X,A8,1X,A8,1X,
-     &         A5,1X,A5,1X,A5,1X,A5)
-22            format(A2)
-
-               iflag =iflag+1
-               j=iflag
- 
-               CALL READMval(M1,lat(j))
-               CALL READMval(M2,lon(j))
-               CALL READMval(M5,dslp(j))
-               CALL READMval(M3,ter(j))
-               CALL READMval(M4,pr(j))
-               CALL READMval(M3,zx(j))
-               CALL READMval(M8,tt(j))
-               CALL READMval(M9,td(j))
-               CALL READMval(M6,d(j))
-               CALL READMval(M7,v(j))
-
-               if(pr(j) .ne. 0 .and. pr(j) 
-     &           .ne. 99999.9 ) then
-                pr(j)= pr(j)/100.
-               end if
-
-               if(dslp(j) .ne. 0 .and. dslp(j) 
-     &           .ne. 99999.9  ) then
-                 dslp(j)= dslp(j)/100.
-               end if
+        if(pres .ne. 0 .and. pres .ne. dumm) then
+            pres=pres/100.
+        endif
+        if(pmsl .ne. 0 .and. pmsl .ne. dumm) then
+            pmsl=pmsl/100.
+        endif
   
-               if(M0 .eq. 'NC000007') then
-                 dname(j)=' METAR'
-               end if
+        if((rpid .ne. dumm) .and. (stsn .ne. dumm)) then
+            write(adpsfcid, '(A,1X,A,1X,A)') 'RPID:',rpid,stsn
+        else if (rpid .ne. dumm) then
+            write(adpsfcid, '(A,1X,A)') 'RPID:',rpid
+        else if (stsn .ne. dumm) then
+            write(adpsfcid, '(A,1X,A)') 'Station name: ',stsn
+        else
+            write(adpsfcid, '(A40)') 'RPID: MISSING'
+        endif
 
-               date(j)=M10
-               mins(j)=M11
-               staid(j)=M20
+c------------------------------------------------------------------------
+c         Write output 
+          if(slat<=lat .and. nlat>=lat .and.
+     +       wlon<=lon .and. elon>=lon) then
+               write(iou,111) isurf,
+     +                        dname,
+     +                        adpsfcid,
+     +                        adpsfcname,
+     +                        adpsfcsource,
+     +                        date,
+     +                        mins,
+     +                        lat,
+     +                        lon,
+     +                        selv,
+     +                        pmsl,
+     +                        nlev,
+     +                        ibogus
+             write(iou,112) pres, selv, tmdb, tmdp, wdir, wspd
+           endif
 
-            ENDDO
+111       format(i1,1x,a6,1x,3(a40,1x),a10,a2,1x,
+     +           3(f20.5,1x),f13.5,1x,i10,1x,i1)
+112       format(6(f13.5,1x))
 
+        ENDDO
 
-
-              CALL READSB  ( 11, ierrsb )
-
-              IF  ( ierrsb .ne. 0 )  THEN
-
-                  msgok = .false.
-
-              ELSE
-              END IF
-
-            END DO
-
-        END DO
-
-1000   if (iflag .ne. 0) then
-
-          write(iou,fmt='(a10)') date_tag
-       do k = 1,iflag
-        if(slat <= lat(k) .and. nlat >= lat(k) .and.
-     &    wlon <= lon(k) .and. elon >= lon(k)) then
-          write(iou,111)isurf,dname(k),staid(k),date(k),mins(k),
-     &    lat(k),
-     &    lon(k),ter(k),dslp(k),nlev,ibogus
-          write(iou,112)pr(k),zx(k),tt(k),td(k),d(k),v(k)
-       endif
-       enddo
-111    format(i1,1x,a6,1x,a6,1x,a10,a2,4(f7.1,1x),i3,1x,i1)
-112    format(6(f7.1,1x))
-
-       endif
-        write(*,*)'nlev ', nlev
-2000  stop 99999
+2000  continue
         
       END
-      SUBROUTINE READMval(M1,fl)
-       character*8 M1
-       dumm=99999.9
-       if(M1(1:1) ==  'm') then
-             fl = dumm
-       else
-             read(M1,*)fl
-       endif
 
+C*-----------------------------------------------------------------------
+       SUBROUTINE get_val(mval, retval)
+
+C      Checks variable value returned by UFBINT and returns either the 
+c      observation value or missing.
+c
+c      Input:
+c         mval: BUFR parameter value returned by UFBINT
+c      Output:
+c         retval: observation value
+
+       real*8 mval
+       real retval
+       parameter(missing=99999.9)
+
+       IF (ibfms(mval) .EQ. 0) THEN
+          retval = mval
+       ELSE
+          retval = missing
+       ENDIF
+       
+       RETURN
+       END
+C*-----------------------------------------------------------------------
+       SUBROUTINE get_lat_lon(clatlon, clatlonh, retval)
+
+C      Get latitude and longitude from either CLAT/CLON or CLATH/CLONH
+c      Input:
+c         clatlon: CLAT or CLON returned by UFBINT
+c         clatlonh: CLATH or CLONH returned by UFBINT
+c      Output:
+c         retval: latitude or longitude value
+
+       real*8 clatlon, clatlonh
+       real retval
+       parameter(missing=99999.9)
+
+       IF (ibfms(clatlon) .EQ. 0) THEN
+          retval = clatlon
+       ELSE IF (ibfms(clatlonh) .EQ. 0) THEN
+          retval = clatlonh
+       ELSE
+          retval = missing
+       ENDIF
+       
        RETURN
        END
