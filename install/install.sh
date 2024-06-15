1b7dda66d58fed31b6ed2eac7e155fe5e27aff5d
Thomas Cram <tcram@ucar.edu>
2024-06-14 18:08:20 -0600

    updated install script

--- install/install.sh
+++ install/install.sh
@@ -1,67 +1,48 @@
 #!/bin/sh
 #  ------------------------------------------------------------------------
-#  This script will make the following executables:
+#  This script will make executables to extract data from ADP BUFR
+#  input files, write the data into a basic text file, and convert
+#  the text files into LITTLE_R format.  It is used to
+#  extract data from these kinds of files:
+#      gdas.adpsfc.tHHz.YYYYMMDD.bufr 
+#      gdas.sfcshp.tHHz.YYYYMMDD.bufr
 #
-#  bufr_sfc2ob.x:       read ADPSFC BUFR input files and
-#                       write output to formatted ASCII files
-#  bufr_ship2ob.x:      read SFCSHIP BUFR input files and 
-#                       write output to formatted ASCII files
-#  dumpbufr.x:          used to dump all contents of a BUFR file.
-#  runob2lit_imd_obs.x: combine and convert obs formatted files into 
-#                       a single file in little_r format
+#  bufr_adpsfc2ob.x: decodes ADPSFC observations and writes to a text file
+#  bufr_sfcshp2ob.x: decodes SFCSHP observations and writes to a text file
+#  surface_obs2littler.x: reads observation text files and converts to little_r format
+#
+#  Note: this script assumes this will be run and compiled on a linux 
+#        system with the Gnu Fortran compiler
 #  ------------------------------------------------------------------------
-
+ 
 set -eua
  
-#  ------------------------------------------------------------------------
-#  CPLAT - platform type (linux,sgi,aix,sun)
-#  ------------------------------------------------------------------------
- 
-CPLAT=linux
 SRC=../src
-LIB=/path/to/BUFRLIB
+LIB=/path/to/libbufr_4.a
+INCL=/path/to/bufrlib/include/directory
 EXE=../exe
 INSTALL=.
-
-#  different platforms use different link name protocols
-#  -----------------------------------------------------
-
-cflag=""
-fflag=""
-
-if [ $CPLAT = linux ]
-then
-   export FC=gfortran
-   export CC=gcc
-   fflag=" -O3 -DUNDERSCORE -fno-second-underscore -w"
-   cflag=" -O3 -DUNDERSCORE -w"
-fi
-
-cd $INSTALL
+ 
+export FC=gfortran
+export CC=gcc
+fflag=" -O3 -DUNDERSCORE -fno-second-underscore -w"
+cflag=" -O3 -DUNDERSCORE -w"
 
 #  Compile the decode programs
-#  ---------------------------------------
- 
-echo "Compiling bufr_decode_ADPsfc programs..."
-$FC $fflag -c $SRC/dumpbufr.f
-$FC $fflag -c $SRC/bufr_sfc2ob.f
-$FC $fflag -c $SRC/bufr_ship2ob.f
-
-$FC $fflag -c $SRC/runob2lit_imd_obs.f
+#  --------------------------------------- 
+echo "Compiling bufr decoding programs..."
+$FC $fflag -c $SRC/bufr_adpsfc2ob.f -I $INCL
+$FC $fflag -c $SRC/bufr_sfcshp2ob.f -I $INCL
+$FC $fflag -c $SRC/surface_obs2littler.f
  
 #  link and load the executables
 #  -----------------------------
-
 echo "Linking..."
-$FC $fflag -o $EXE/dumpbufr.x dumpbufr.o $LIB/bufrlib.a
+$FC $fflag -o $EXE/bufr_adpsfc2ob.x bufr_adpsfc2ob.o $LIB -I $INCL
+$FC $fflag -o $EXE/bufr_sfcshp2ob.x bufr_sfcshp2ob.o $LIB -I $INCL
+$FC $fflag -o $EXE/surface_obs2littler.x surface_obs2littler.o
 
-$FC $fflag -o $EXE/bufr_sfc2ob.x bufr_sfc2ob.o $LIB/bufrlib.a
-$FC $fflag -o $EXE/bufr_ship2ob.x bufr_ship2ob.o $LIB/bufrlib.a
-
-$FC $fflag -o $EXE/runob2lit_imd_obs.x runob2lit_imd_obs.o
 #  clean up
 #  --------
-
-rm -f *.o
-
+rm -f *.o *.mod
 echo "Finished."
